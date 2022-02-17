e7b254ec2371da4ab711ff09e2bda26a4feeca34
mrlutik <yaievgeniy@gmail.com>
2022-02-17 12:12:39 +0200

    exec bit + make publish

--- scripts/nfpm.sh
+++ scripts/nfpm.sh
@@ -10,4 +10,5 @@ wget https://go.dev/dl/go1.17.7.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 rm go1.17.7.linux-amd64.tar.gz
-../make publish
+cd ..
+make publish
