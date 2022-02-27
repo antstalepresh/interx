ecbbe803df0fa95b69f04839722289c1d26febb7
asmodat <asmodat@gmail.com>
2022-02-27 17:53:48 +0000

    Add support for gocosmos protogen

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -36,9 +36,10 @@ cd $CURRENT_DIR
 loadGlobEnvs
 
 go clean -modcache
+EXPECTED_INTERX_PROTO_DEP_VER="v0.0.1"
 BUF_VER=$(buf --version 2> /dev/null || echo "")
 
-if ($(isNullOrEmpty "$BUF_VER")) ; then
+if ($(isNullOrEmpty "$BUF_VER")) || [ "$INTERX_PROTO_DEP_VER" != "$EXPECTED_INTERX_PROTO_DEP_VER" ] ; then
     GO111MODULE=on 
     go install github.com/bufbuild/buf/cmd/buf@v1.0.0-rc10
     echoInfo "INFO: Sucessfully intalled buf $(buf --version)"
@@ -54,15 +55,18 @@ if ($(isNullOrEmpty "$BUF_VER")) ; then
      go install github.com/gogo/protobuf/protoc-gen-gogofast@v${GOGO_PROTOBUF_VERSION} && \
      go install github.com/gogo/protobuf/protoc-gen-gogofaster@v${GOGO_PROTOBUF_VERSION} && \
      go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v${GRPC_GATEWAY_VERSION} && \
-     go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v${GRPC_GATEWAY_VERSION}
+     go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v${GRPC_GATEWAY_VERSION} && \
+     go install github.com/gogo/protobuf/protoc-gen-gogotypes
 
     # Following command executes with error requiring us to silence it, however the executable is placed in $GOBIN
     # https://github.com/regen-network/cosmos-proto
+    # Original setup originates from Docker Image tendermintdev/sdk-proto-gen:v0.2
     # reference: 
     go install github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@v0.3.1 2> /dev/null || : 
     go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@latest
 
     go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
+    setGlobEnv INTERX_PROTO_DEP_VER "$EXPECTED_INTERX_PROTO_DEP_VER"
 fi
 
 COSMOS_BRANCH="v0.45.1"
@@ -80,9 +84,19 @@ cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk@$COSMOS
 rm -rfv ./proto/cosmos ./proto/kira ./third_party/proto
 mkdir -p ./third_party/proto
 cp -rfv $cosmos_sdk_dir/proto/cosmos ./proto
-cp -rfv $cosmos_sdk_dir/third_party/proto/cosmos_proto ./third_party/proto
-cp -rfv $cosmos_sdk_dir/third_party/proto/tendermint ./third_party/proto
+cp -rfv $cosmos_sdk_dir/third_party/proto/* ./third_party/proto
 cp -rfv $kira_dir/proto/kira ./proto
+
+### This part is required by gocosmos_out
+rm -rfv ./codec && mkdir -p codec/types
+buf protoc -I "third_party/proto" --gogotypes_out=./codec/types third_party/proto/google/protobuf/any.proto
+mv codec/types/google/protobuf/any.pb.go codec/types
+rm -rfv codec/types/third_party
+rm -rfv ./third_party/proto/gogoproto
+rm -rfv ./third_party/proto/google
+###
+
+sed '/proto\.RegisterType/d' codec/types/any.pb.go > tmp && mv tmp codec/types/any.pb.go
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 
@@ -98,7 +112,9 @@ for dir in $proto_dirs; do
 		  -I third_party/proto/ \
           --go_out=paths=source_relative:./proto-gen \
           --go-grpc_out=paths=source_relative:./proto-gen \
-          --grpc-gateway_out=paths=source_relative:./proto-gen \
+          --gocosmos_out=plugins=interfacetype+grpc,\
+Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:./proto-gen \
+          --grpc-gateway_out=logtostderr=true,paths=source_relative:./proto-gen \
           $fil || ( echoErr "ERROR: Failed proto build for: ${fil}" && sleep 2 && exit 1 )
     done
 done
