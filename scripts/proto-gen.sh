64f696be02e21cbd1999b2502ad160cf33edeb09
asmodat <asmodat@gmail.com>
2022-03-01 11:03:00 +0000

    Allow go root to execute

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -26,7 +26,8 @@ if  ($(isNullOrEmpty "$GO_VER")) || ($(isNullOrEmpty "$GOBIN")) ; then
      setGlobEnv GOPATH "/home/go" && setGlobPath "\$GOPATH" && \
      setGlobEnv GOCACHE "/home/go/cache" && \
      loadGlobEnvs && \
-     mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH"
+     mkdir -p "$GOPATH/src" "$GOPATH/bin" && \
+     chmod -R 777 "$GOPATH" && chmod -R 777 "$GOROOT"
 
     echoInfo "INFO: Sucessfully intalled $(go version)"
 fi
