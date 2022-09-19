ec4ca32ebe82f790381909a8702137116f8e5a30
fantasyup <fantasy99483@gmail.com>
2022-09-19 08:49:19 -0400

    add write contract endpoint

--- gateway/evm/contract.go
+++ gateway/evm/contract.go
@@ -24,8 +24,10 @@ import (
 // RegisterEVMContractRoutes registers read/write of smart contract.
 func RegisterEVMContractRoutes(r *mux.Router, rpcAddr string) {
 	r.HandleFunc(config.QueryReadContract, QueryReadContractRequest(rpcAddr)).Methods("GET")
+	r.HandleFunc(config.QueryWriteContract, QueryWriteContractRequest(rpcAddr)).Methods("GET")
 
 	common.AddRPCMethod("GET", config.QueryReadContract, "This is an API to read smart contract.", true)
+	common.AddRPCMethod("GET", config.QueryWriteContract, "This is an API to write smart contract.", true)
 }
 
 func ReadContractCall(nodeInfo config.EVMNodeConfig, call *EVMCall, method abi.Method) (interface{}, error) {
@@ -227,5 +229,207 @@ func QueryReadContractRequest(rpcAddr string) http.HandlerFunc {
 		}
 
 		common.WrapResponse(w, request, *response, statusCode, common.RPCMethods["GET"][config.QueryReadContract].CachingEnabled)
+	}
+}
+
+func WriteContractCall(nodeInfo config.EVMNodeConfig, from string, contract string, value string, data string) (interface{}, interface{}, int) {
+	client := jsonrpc2.NewRPCClient(nodeInfo.RPC + "/" + nodeInfo.RPCToken)
+	if nodeInfo.RPCSecret != "" {
+		client.SetBasicAuth(nodeInfo.RPCToken, nodeInfo.RPCSecret)
+	}
+
+	type UnsignedTransaction struct {
+		From     string `json:"from"`
+		To       string `json:"to"`
+		ChainId  uint64 `json:"chainId"`
+		Nonce    uint64 `json:"nonce"`
+		GasLimit uint64 `json:"gasLimit"`
+		GasPrice uint64 `json:"gasPrice"`
+		Value    string `json:"value"`
+		Data     string `json:"data"`
+	}
+
+	var response UnsignedTransaction
+	response.From = from
+	response.To = contract
+	response.Value = value
+	response.Data = data
+
+	result, err := client.Call("eth_gasPrice")
+	if err != nil {
+		return common.ServeError(0, "failed to get gas price ", err.Error(), http.StatusInternalServerError)
+	}
+	gasPrice, err := result.GetString()
+	if err != nil {
+		return common.ServeError(0, "failed to get gas price ", err.Error(), http.StatusInternalServerError)
+	}
+	response.GasPrice, _ = strconv.ParseUint((gasPrice)[2:], 16, 64)
+
+	result, err = client.Call("eth_chainId")
+	if err != nil {
+		return common.ServeError(0, "failed to get chain id", err.Error(), http.StatusInternalServerError)
+	}
+	chainId, err := result.GetString()
+	if err != nil {
+		return common.ServeError(0, "failed to get chain id", err.Error(), http.StatusInternalServerError)
+	}
+	response.ChainId, _ = strconv.ParseUint((chainId)[2:], 16, 64)
+
+	result, err = client.Call("eth_getTransactionCount", from, "latest")
+	if err != nil {
+		return common.ServeError(0, "failed to get latest sequence", err.Error(), http.StatusInternalServerError)
+	}
+	sequence, err := result.GetString()
+	if err != nil {
+		return common.ServeError(0, "failed to get latest sequence", err.Error(), http.StatusInternalServerError)
+	}
+	response.Nonce, _ = strconv.ParseUint((sequence)[2:], 16, 64)
+
+	type TransactionCall struct {
+		From     string `json:"from"`
+		To       string `json:"to"`
+		GasPrice string `json:"gasPrice"`
+		Value    string `json:"value"`
+		Data     string `json:"data"`
+	}
+	var transactionCall TransactionCall
+	transactionCall.From = from
+	transactionCall.To = contract
+	transactionCall.GasPrice = gasPrice
+	transactionCall.Value = "0x" + hex.EncodeToString([]byte(value))
+	transactionCall.Data = data
+	common.GetLogger().Info(transactionCall)
+
+	result, err = client.Call("eth_estimateGas", transactionCall, "latest")
+	if err != nil {
+		return common.ServeError(0, "failed to estimate gas", err.Error(), http.StatusInternalServerError)
+	}
+	gasLimit, err := result.GetString()
+	if err != nil {
+		return common.ServeError(0, "failed to get gas limit ", err.Error(), http.StatusInternalServerError)
+	}
+	response.GasLimit, _ = strconv.ParseUint((gasLimit)[2:], 16, 64)
+
+	return response, nil, http.StatusOK
+}
+
+func queryWriteSmartContractHandle(r *http.Request, chain string, contract string) (interface{}, interface{}, int) {
+	isSupportedChain, chainConfig := GetChainConfig(chain)
+	if !isSupportedChain {
+		return common.ServeError(0, "", "unsupported chain", http.StatusBadRequest)
+	}
+
+	_ = r.ParseForm()
+
+	functionName := r.FormValue("function")
+	if len(functionName) == 0 {
+		return common.ServeError(0, "", "no function name", http.StatusBadRequest)
+	}
+
+	from := r.FormValue("from")
+	if len(from) == 0 {
+		return common.ServeError(0, "", "no from address", http.StatusBadRequest)
+	}
+
+	value := r.FormValue("value")
+
+	var abiDecoded abi.ABI
+	abiJsonUrlEncoded := r.FormValue("abi")
+	if len(abiJsonUrlEncoded) == 0 {
+		result, err, statusCode := common.MakeGetRequest(chainConfig.Etherscan.API, "", "module=contract&action=getabi&address="+contract+"&apikey="+chainConfig.Etherscan.APIToken)
+		if err != nil {
+			return nil, err, statusCode
+		}
+
+		abiDecoded, err = abi.JSON(strings.NewReader(result.(map[string]interface{})["result"].(string)))
+		if err != nil {
+			return nil, err, http.StatusBadRequest
+		}
+	} else {
+		abiJson, err := url.QueryUnescape(abiJsonUrlEncoded)
+		if err != nil {
+			return common.ServeError(0, "Invalid ABI JSON", err.Error(), http.StatusBadRequest)
+		}
+
+		abiDecoded, err = abi.JSON(strings.NewReader(abiJson))
+		if err != nil {
+			return common.ServeError(0, "Invalid ABI JSON", err.Error(), http.StatusBadRequest)
+		}
+	}
+
+	method, exist := abiDecoded.Methods[functionName]
+	if !exist {
+		return common.ServeError(0, "", "function does not exist", http.StatusBadRequest)
+	}
+
+	argLen := len(method.Inputs)
+	var args []interface{}
+	for i := 0; i < argLen; i++ {
+		arg := r.FormValue("key_" + strconv.Itoa(i+1))
+		if len(arg) == 0 {
+			return common.ServeError(0, "", "argument invalid", http.StatusBadRequest)
+		}
+
+		v, err := decodeAbiArgument(arg, &method.Inputs[i])
+		if err != nil {
+			return common.ServeError(0, "", "argument invalid", http.StatusBadRequest)
+		}
+
+		args = append(args, v)
+	}
+
+	bytes, err := abiDecoded.Pack(functionName, args...)
+	if err != nil {
+		return common.ServeError(0, "", err.Error(), http.StatusBadRequest)
+	}
+
+	res, fail, statusCode := WriteContractCall(chainConfig.QuickNode, from, contract, value, "0x"+hex.EncodeToString(bytes))
+	if fail == nil {
+		return res, fail, statusCode
+	}
+
+	res, fail, statusCode = WriteContractCall(chainConfig.Infura, from, contract, value, "0x"+hex.EncodeToString(bytes))
+	if fail == nil {
+		return res, fail, statusCode
+	}
+
+	res, fail, statusCode = WriteContractCall(chainConfig.Pokt, from, contract, value, "0x"+hex.EncodeToString(bytes))
+	if fail == nil {
+		return res, fail, statusCode
+	}
+
+	return res, fail, statusCode
+}
+
+// QueryWriteContractRequestf is a function to write smart contract.
+func QueryWriteContractRequest(rpcAddr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		queries := mux.Vars(r)
+		chain := queries["chain"]
+		contract := queries["contract"]
+		request := common.GetInterxRequest(r)
+		response := common.GetResponseFormat(request, rpcAddr)
+		statusCode := http.StatusOK
+
+		common.GetLogger().Info("[query-evm-write-contract] Entering write smart contract: ", chain)
+
+		if !common.RPCMethods["GET"][config.QueryWriteContract].Enabled {
+			response.Response, response.Error, statusCode = common.ServeError(0, "", "API disabled", http.StatusForbidden)
+		} else {
+			if common.RPCMethods["GET"][config.QueryWriteContract].CachingEnabled {
+				found, cacheResponse, cacheError, cacheStatus := common.SearchCache(request, response)
+				if found {
+					response.Response, response.Error, statusCode = cacheResponse, cacheError, cacheStatus
+					common.WrapResponse(w, request, *response, statusCode, false)
+
+					common.GetLogger().Info("[query-evm-write-contract] Returning from the cache: ", chain)
+					return
+				}
+			}
+
+			response.Response, response.Error, statusCode = queryWriteSmartContractHandle(r, chain, contract)
+		}
+
+		common.WrapResponse(w, request, *response, statusCode, common.RPCMethods["GET"][config.QueryWriteContract].CachingEnabled)
 	}
 }
